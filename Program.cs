e3531bcfbba82ef9ef2fbd06754c4000a2a01e59
LinkWhale <filipbjed@gmail.com>
2023-12-06 14:26:25 +0100

    comments

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ public class Profile {
     public int Age {get; set;}
     public string City {get; set;}
     public string Country {get; set;}
-    public List<string> Hobbies {get; set;}
+    public List<string> Hobbies {get; set;} //List that takes string as elements
 
     //initializer
     public Profile(string name, int age, string city, string country) {
@@ -17,10 +17,12 @@ public class Profile {
         Hobbies = new List<string>();
     }
 
+    //Method to add a hobby
     public void add_hobby(string hobby){
         Hobbies.Add(hobby);
     }
 
+    //Method that views profile
     public void view_profile() {
         Console.WriteLine("");
         Console.WriteLine($"Name: {Name}");
@@ -29,7 +31,7 @@ public class Profile {
         Console.WriteLine($"Country: {Country}");
         Console.WriteLine("Hobbies:");
         Console.WriteLine("");
-        for(int i = 0; i < Hobbies.Count(); i++ ){
+        for(int i = 0; i < Hobbies.Count(); i++ ){ //print out the whole list
             Console.WriteLine(Hobbies[i]);
         }
         Console.WriteLine("");
@@ -38,6 +40,8 @@ public class Profile {
 
 class Program {
     public static void Main(){
+
+        //establish your account
         Console.WriteLine("Input your name");
         string username = Console.ReadLine();
         Console.WriteLine("Input your age");
@@ -47,19 +51,20 @@ class Program {
         Console.WriteLine("Input your country");
         string country = Console.ReadLine();
 
-        Profile newProfile = new Profile(username, age, city, country);
+        Profile newProfile = new Profile(username, age, city, country); //initialize the class
 
+        //main loop
         while(true){
             newProfile.view_profile();
             Console.WriteLine("What do you want to do?");
             Console.WriteLine("1. Add hobby");
             Console.WriteLine("2. Exit Program");
             int choice = Convert.ToInt32(Console.ReadLine());
-            if(choice == 1) {
+            if(choice == 1) { //choice 1 will add a custom hobby
                 Console.WriteLine("Name of your hobby: ");
                 newProfile.add_hobby(Console.ReadLine());
             }
-            if(choice == 2) {
+            if(choice == 2) { //choice 2 will exit the program
                 break;
             }
             else {
